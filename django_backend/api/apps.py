243fc91ac53c722c1e1be634b515987a40087714
emerdenny <emerdenny@protonmail.ch>
2022-05-28 14:51:11 -0400

    restructure project

--- django_backend/api/apps.py
+++ django_backend/api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SeqalignConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'seqAlign'
+    name = 'api'
--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -1,4 +1,4 @@
-"""django_api URL Configuration
+"""django_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from seqAlign import views
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
